b0667f69dda6ff7ae2fe4f32f9d0657d311768c3
jorge-73 <jorge_ap_73@hotmail.com>
2024-03-14 20:30:40 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://real-time-chat-one-lemon.vercel.app",
     credentials: true,
   })
 );
